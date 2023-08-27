edbb51a2d0d757cc869ed1320204209b6f031218
yite <coastq22889@icloud.com>
2023-08-27 02:18:26 +0000

    exhost_manager

--- exhost/binancefuture/rest_manager.py
+++ exhost/binancefuture/rest_manager.py
@@ -1,7 +1,17 @@
+import os
+import json
 from fastapi import FastAPI
+
+from config.binancefuture_kucoin_arb import LOG_DIR, TIMESTAMP
+from logger import setup_logger
+
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
+logger = setup_logger(__name__, os.path.join(LOG_DIR, f"{TIMESTAMP}_{__name__}.log"))
+logger.info(f"init {__name__}")
+
+@app.get("/test")
+async def test():
+    logger.info("test is call!")
     return {"Hello": "World"}
