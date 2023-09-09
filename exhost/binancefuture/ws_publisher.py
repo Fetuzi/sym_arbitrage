29dbe669605b0082f4209950f05d77a7113bef39
yite <coastq22889@icloud.com>
2023-09-10 07:01:00 +0800

    add try catch

--- exhost/binancefuture/ws_publisher.py
+++ exhost/binancefuture/ws_publisher.py
@@ -6,20 +6,38 @@ import redis
 from general.logger import setup_logger
 from config.binancefuture_okx_arb import TIMESTAMP, LOG_DIR, REDIS_HOST, REDIS_PORT, REDIS_PUBSUB, BINANCE, TOKYO_WS
 
+MAX_RETRIES = 5  # Set maximum retries
+RETRY_WAIT_TIME = 5  # Wait time between each retry in seconds
+TIMEOUT = 3  # 3 seconds timeout for receiving data
+
 NAME = os.path.splitext(os.path.basename(__file__))[0]
 logger = setup_logger(NAME, os.path.join(LOG_DIR, f"{TIMESTAMP}_{NAME}_{REDIS_PUBSUB}.log"))
 logger.info(f"init {NAME}")
 
-
 async def subscribe_to_websocket(uri, redis_client):
-    async with websockets.connect(uri) as websocket:
+    retries = 0
+    while retries < MAX_RETRIES:
+        try:
+            async with websockets.connect(uri) as websocket:
+                retries = 0  # Reset retries if successful connection
+                while True:
+                    try:
+                        message = await asyncio.wait_for(websocket.recv(), timeout=TIMEOUT)
+                        data = json.loads(message)
+                        logger.info(f"Received data: {data}")
 
-        async for message in websocket:
-            data = json.loads(message)
-            logger.info(f"Received data: {data}")
-            # Publish to Redis
-            redis_client.publish(REDIS_PUBSUB, json.dumps({'ex': BINANCE, 'b': data['b'][0], 'a': data['a'][0]}))
+                        # Publish to Redis
+                        redis_client.publish(REDIS_PUBSUB, json.dumps({'ex': BINANCE, 'b': data['b'][0], 'a': data['a'][0]}))
+                    except asyncio.TimeoutError:
+                        logger.warning("No data received for 3 seconds, retrying...")
+                        break
+        except Exception as e:
+            logger.error(f"Cannot connect to WebSocket server: {e}")
+            retries += 1
+            logger.info(f"Retrying in {RETRY_WAIT_TIME} seconds...")
+            await asyncio.sleep(RETRY_WAIT_TIME)
 
+    logger.error(f"Reached max retries {MAX_RETRIES}. Exiting.")
 
 if __name__ == '__main__':
     # Initialize Redis client
