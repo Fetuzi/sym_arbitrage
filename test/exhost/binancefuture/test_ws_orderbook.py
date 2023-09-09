31b8184c3c82be82b7ea1307d8e20061dfb3156f
yite <coastq22889@icloud.com>
2023-09-09 23:25:20 +0800

    add publisher

--- test/exhost/binancefuture/test_ws_orderbook.py
+++ test/exhost/binancefuture/test_ws_orderbook.py
@@ -6,8 +6,8 @@ from general.logger import setup_logger
 
 
 class TestWebSocketRelay(unittest.IsolatedAsyncioTestCase):
-    WEBSOCKET_URL = 'ws://127.0.0.1:8765'  # Replace with your WebSocket relay server URL
-    # WEBSOCKET_URL = 'ws://ec2-52-198-41-165.ap-northeast-1.compute.amazonaws.com:8765'
+    # WEBSOCKET_URL = 'ws://127.0.0.1:8765'  # Replace with your WebSocket relay server URL
+    WEBSOCKET_URL = 'ws://ec2-52-198-41-165.ap-northeast-1.compute.amazonaws.com:8765'
 
     # WEBSOCKET_URL = 'ws://ec2-16-163-156-120.ap-east-1.compute.amazonaws.com:8765'
     logger = setup_logger(__name__, None, logging.INFO)
